a56fb3a528dd0fc960437bcec93ba04b0c0d44bd
Giorgio <1999giorgio.monne@mail.com>
2022-02-17 22:34:53 -1000

    implementazione bottoni con rotte

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    Boolpress
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -32,7 +32,15 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                    <ul class="navbar-nav ">
+                        
+                        <a class="nav-link" href="{{route("home")}}">Dashboard</a>
+
+                    </ul>
+
+                    <ul class="navbar-nav ">
+                        
+                        <a class="nav-link" href="{{route("posts.index")}}">Posts</a>
 
                     </ul>
 
