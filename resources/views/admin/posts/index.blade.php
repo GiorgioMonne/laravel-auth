a13c767dfc48a99725ac9c17d7091a8514de5278
Giorgio <1999giorgio.monne@mail.com>
2022-02-17 23:31:10 -1000

    visualizzazione del post con stato

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -9,11 +9,28 @@
                 <div class="card-header">{{ __('Lista Posts') }}</div>
 
                 <div class="card-body">
-                    <ul>
-                        @foreach ($posts as $post)
-                            <li>{{$post->title}}</li>
-                        @endforeach
-                    </ul>
+
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Titolo</th>
+                            <th scope="col">Slug</th>
+                            <th scope="col">Azioni</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          @foreach ($posts as $post)
+                          <tr>
+                            <td>{{$post->id}}</td>
+                            <td>{{$post->title}}</td>
+                            <td>{{$post->slug}}</td>
+                            <td><a href="{{route("posts.show", $post->id)}}"><button type="button" class="btn btn-info">Info</button></a></td>
+                          </tr>
+                          @endforeach
+                        </tbody>
+                      </table>
+
                 </div>
             </div>
         </div>
